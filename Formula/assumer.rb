9f65a762e1f114893ff4f6d3051af28571b474f4
J. Fulbright <jeremiah@iclasspro.com>
2026-05-14 11:21:49 -0500

    Bump assumer to 1.0.5

--- Formula/assumer.rb
+++ Formula/assumer.rb
@@ -2,8 +2,8 @@ class Assumer < Formula
   desc "Terminal UI for AWS SSO authentication and role assumption"
   homepage "https://github.com/j-fulbright/rust-aws-sso-tui"
   url "https://github.com/j-fulbright/rust-aws-sso-tui/archive/refs/heads/main.tar.gz"
-  sha256 "ffec3b0a396357e6a9e5a25c8719029eadbb87605438105b547e5c7237ea7d7f"
-  version "1.0.4"
+  sha256 "2239f296254be43cf8ad823ff2375113b7db99729463041ea166a6c81194a6f3"
+  version "1.0.5"
   license "MIT"
 
   depends_on "rust" => :build
